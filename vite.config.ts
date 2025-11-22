54d9a8b64fc16d4d3ebf0a98ccb41d793fd37909
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-11-22 12:58:14 +0700

    configure vite

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'node:path';
+import { cloudflare } from '@cloudflare/vite-plugin';
+import { reactRouter } from '@react-router/dev/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [cloudflare({ viteEnvironment: { name: 'ssr' } }), reactRouter()],
+	resolve: {
+		alias: {
+			'@': path.resolve(import.meta.dirname, '.'),
+		},
+	},
+});
