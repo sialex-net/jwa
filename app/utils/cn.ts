cdbc038e57ef9aa68e5b66b3c17c6087800f732a
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-11-25 06:17:27 +0700

    add cn fn
    * app/utils/cn.ts

---
+++ app/utils/cn.ts
@@ -0,0 +1,13 @@
+import type { ClassValue } from 'cva';
+import { cx } from 'cva';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Merges Tailwind class names, resolving any conflicts.
+ *
+ * @param inputs - An array of class names to merge.
+ * @returns A string of merged and optimized class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
+	return twMerge(cx(inputs));
+}
