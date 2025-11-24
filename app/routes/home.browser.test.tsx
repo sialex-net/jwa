7a0929291db2f55a56bf1feff928edaeba206872
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-11-25 06:17:25 +0700

    add unit test to root index route

---
+++ app/routes/home.browser.test.tsx
@@ -0,0 +1,22 @@
+import { describe, expect, test } from 'vitest';
+import { default as routeDefault } from './home';
+
+describe('home route', () => {
+	// biome-ignore lint/suspicious/noTsIgnore: it exists
+	// @ts-ignore: Property 'renderStub' does not exist on type 'TestContext'
+	test('should render page text', async ({ renderStub }) => {
+		let rendered = await renderStub({
+			entries: [
+				{
+					// @ts-expect-error
+					Component: () => routeDefault(),
+					path: '/',
+				},
+			],
+			props: { initialEntries: ['/'] },
+		});
+
+		let actual = rendered.getByText('Hello world').element().textContent;
+		expect(actual).toMatch(/world/);
+	});
+});
