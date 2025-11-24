844f4d45fb54c61f24b8a2715ddfe5f10282359a
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-11-25 06:17:29 +0700

    configure iconsSpritesheet fn in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { iconsSpritesheet } from 'vite-plugin-icons-spritesheet';
 
 export default defineConfig({
 	plugins: [
 		cloudflare({ viteEnvironment: { name: 'ssr' } }),
+		iconsSpritesheet({
+			fileName: 'sprite.svg',
+			iconNameTransformer: (name) => name,
+			inputDir: './config/svg-icons',
+			outputDir: './app/components/ui/icons',
+			withTypes: true,
+		}),
 		reactRouter(),
 		tailwindcss(),
 	],
