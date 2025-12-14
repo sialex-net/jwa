054f37ba663c3df24de29bd77485f72cb0d4e7f3
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-14 15:56:14 +0700

    render APP_ENV and TURSO_URL on `/users` route in localhost

--- app/routes/users/home.tsx
+++ app/routes/users/home.tsx
@@ -1,10 +1,15 @@
 import { drizzle } from 'drizzle-orm/libsql';
+import { appContext } from '@/app/context';
 import { getClientCf } from '@/app/middleware/libsql';
 import * as schema from '@/data/drizzle/schema';
 /** @knipIgnoreUnresolved */
 import type { Route } from './+types/home';
 
-export async function loader(_: Route.LoaderArgs) {
+export async function loader({ context, request }: Route.LoaderArgs) {
+	let appEnv = context.get(appContext).env.APP_ENV;
+	let tursoUrl = context.get(appContext).env.TURSO_URL;
+	let url = new URL(request.url);
+
 	let client = getClientCf();
 	let db = drizzle(client, { logger: true, schema });
 
@@ -14,7 +19,10 @@ export async function loader(_: Route.LoaderArgs) {
 
 	return {
 		data: {
+			appEnv,
+			hostname: url.hostname,
 			query,
+			tursoUrl,
 		},
 	};
 }
@@ -28,6 +36,12 @@ export default function Component({ loaderData }: Route.ComponentProps) {
 					<li key={user.id}>{user.username}</li>
 				))}
 			</ul>
+			{loaderData.data.hostname === 'localhost' && (
+				<div>
+					<p>APP_ENV={loaderData.data.appEnv}</p>
+					<p>TURSO_URL={loaderData.data.tursoUrl}</p>
+				</div>
+			)}
 		</>
 	);
 }
