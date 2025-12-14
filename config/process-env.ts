d971637fbe524aac33d58f31de8bcdc6396a3eb8
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-14 15:55:57 +0700

    rm APP_ENV from processEnv in config/process-env.ts

--- config/process-env.ts
+++ config/process-env.ts
@@ -2,11 +2,9 @@ import { z } from 'zod';
 
 const processEnv = z
 	.object({
-		APP_ENV: z.union([z.literal('development'), z.literal('production')]),
-		LOCAL_TURSO_URL: z.url(),
 		TURSO_AUTH_TOKEN: z.string(),
 		TURSO_URL: z.url(),
 	})
 	.parse(process.env);
 
-export const { APP_ENV, TURSO_AUTH_TOKEN, TURSO_URL } = processEnv;
+export const { TURSO_AUTH_TOKEN, TURSO_URL } = processEnv;
