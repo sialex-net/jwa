c4b89f46ae2d0c2a27fbd4434e98d73e5ad967bb
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-14 15:51:09 +0700

    render users from database

--- app/routes/users/home.tsx
+++ app/routes/users/home.tsx
@@ -1,6 +1,33 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { getClientCf } from '@/app/middleware/libsql';
+import * as schema from '@/data/drizzle/schema';
 /** @knipIgnoreUnresolved */
 import type { Route } from './+types/home';
 
-export default function Component(_: Route.ComponentProps) {
-	return <h1>Users</h1>;
+export async function loader(_: Route.LoaderArgs) {
+	let client = getClientCf();
+	let db = drizzle(client, { logger: true, schema });
+
+	let query = await db.select().from(schema.users);
+
+	client.close();
+
+	return {
+		data: {
+			query,
+		},
+	};
+}
+
+export default function Component({ loaderData }: Route.ComponentProps) {
+	return (
+		<>
+			<h1>Users</h1>
+			<ul>
+				{loaderData.data.query.map((user) => (
+					<li key={user.id}>{user.username}</li>
+				))}
+			</ul>
+		</>
+	);
 }
