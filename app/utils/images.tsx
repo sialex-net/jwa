e39e192cfbed9c50da77b34576b27ddac314cdca
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-26 06:59:59 +0700

    add helper fn to get post image

--- app/utils/images.tsx
+++ app/utils/images.tsx
@@ -3,3 +3,7 @@ import userFallback from '@/app/assets/images/user.jpeg';
 export function getUserImgSrc(imageId?: null | string) {
 	return imageId ? `/resources/user-images/${imageId}` : userFallback;
 }
+
+export function getPostImgSrc(imageId: string) {
+	return `/resources/post-images/${imageId}`;
+}
