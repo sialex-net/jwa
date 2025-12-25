df7625a5de57b4735f6f9b8287410a948f88d2d2
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-26 06:59:59 +0700

    add floatingToolbarClassName variable

---
+++ app/components/floating-toolbar.tsx
@@ -0,0 +1,2 @@
+export const floatingToolbarClassName =
+	'absolute right-3 bottom-3 left-3 flex items-center justify-end gap-2 rounded-lg bg-muted/80 p-4 pl-5 shadow-accent shadow-xl backdrop-blur-sm md:gap-4 md:pl-7';
