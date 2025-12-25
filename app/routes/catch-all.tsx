df324c3314f212708b5c655e581fe3b893cbc020
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-26 06:59:51 +0700

    update catchall route

--- app/routes/catch-all.tsx
+++ app/routes/catch-all.tsx
@@ -1,16 +1,41 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
+import { GeneralErrorBoundary } from '../components/error-boundary';
+import { Icon } from '../components/ui/icon';
 /** @knipIgnoreUnresolved */
 import type { Route } from './+types/catch-all';
 
-export default function Component(_: Route.ComponentProps) {
-	return (
-		<>
-			<div>
-				<h1>404</h1>
-				<p>Page Not Found</p>
-			</div>
+export function loader() {
+	throw new Response('Not found', { status: 404 });
+}
 
-			<Link to="/">Go to Homepage</Link>
-		</>
+export function action() {
+	throw new Response('Not found', { status: 404 });
+}
+
+// fallback if loader fails
+export default function (_: Route.ComponentProps) {
+	return <ErrorBoundary />;
+}
+
+export function ErrorBoundary() {
+	let location = useLocation();
+	return (
+		<GeneralErrorBoundary
+			statusHandlers={{
+				404: () => (
+					<main className="flex flex-col gap-y-6">
+						<div className="flex flex-col gap-y-3">
+							<h1>We can't find this page:</h1>
+							<pre className="whitespace-pre-wrap break-all">
+								{location.pathname}
+							</pre>
+						</div>
+						<Link to="/">
+							<Icon name="arrow-left">Back to home</Icon>
+						</Link>
+					</main>
+				),
+			}}
+		/>
 	);
 }
