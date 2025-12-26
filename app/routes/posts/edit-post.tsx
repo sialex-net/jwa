a55163adbd524fe528a7435fa7d6e83cdfe784d9
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-26 07:00:03 +0700

    add edit post functionality

--- app/routes/posts/edit-post.tsx
+++ app/routes/posts/edit-post.tsx
@@ -1,5 +1,57 @@
+import { invariantResponse } from '@epic-web/invariant';
+import { eq, sql } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/libsql';
+import { getClientCf } from '@/app/middleware/libsql';
+import * as schema from '@/data/drizzle/schema';
 import type { Route } from './+types/edit-post';
+import { PostEditor } from './post-editor';
 
-export default function Component({ params }: Route.ComponentProps) {
-	return <h1>Edit post: {params.postId} </h1>;
+export { action } from './post-editor.server';
+
+export async function loader({ params }: Route.LoaderArgs) {
+	let client = getClientCf();
+	let db = drizzle({ client, logger: true, schema });
+
+	let query = await db
+		.select({
+			content: schema.posts.content,
+			createdAt: schema.posts.createdAt,
+			id: schema.posts.id,
+			images: {
+				altText: sql<null | string>`${schema.postImages.altText}`,
+				id: schema.postImages.id,
+			},
+			title: schema.posts.title,
+		})
+		.from(schema.posts)
+		.where(eq(schema.posts.id, params.postId))
+		.leftJoin(schema.postImages, eq(schema.posts.id, schema.postImages.postId));
+
+	client.close();
+
+	invariantResponse(query, 'Post not found', { status: 404 });
+
+	return {
+		data: {
+			post: {
+				content: query[0].content,
+				created: query[0].createdAt,
+				id: query[0].id,
+				images: query[0].images ? query.map((item) => item.images) : [],
+				title: query[0].title,
+			},
+		},
+	};
+}
+
+export default function Component({
+	actionData,
+	loaderData,
+}: Route.ComponentProps) {
+	return (
+		<PostEditor
+			actionData={actionData}
+			post={loaderData.data.post}
+		/>
+	);
 }
