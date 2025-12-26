4bf8de71c381ae55c5859f66736dfefadb189515
sialex-net <91857463+sialex-net@users.noreply.github.com>
2025-12-26 07:10:06 +0700

    wrap content in main element (a11y) on root index route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,36 +6,38 @@ export default function Component(_: Route.ComponentProps) {
 	return (
 		<div className="flex flex-col items-center gap-y-8 py-32 pt-24">
 			<h1 className="font-semibold text-3xl">john wicki</h1>
-			<figure>
-				<img
-					alt="A poster of John Wick"
-					className="h-auto w-104"
-					src={heroImage}
-				/>
-				<figcaption className="py-2 text-sm">
-					Source:{' '}
-					<a
-						href="https://displate.com"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						Displate
-					</a>
-				</figcaption>
-			</figure>
-			<section>
-				<h2 className="py-8 font-semibold text-xl">Film Summaries</h2>
-				<ol className="flex flex-col gap-y-8">
-					{filmSummaries.map((film) => (
-						<li key={film.id}>
-							<div>
-								<h3 className="py-2 font-semibold underline">{film.title}</h3>
-								<p>{film.summary}</p>
-							</div>
-						</li>
-					))}
-				</ol>
-			</section>
+			<main>
+				<figure>
+					<img
+						alt="A poster of John Wick"
+						className="h-auto w-104"
+						src={heroImage}
+					/>
+					<figcaption className="py-2 text-sm">
+						Source:{' '}
+						<a
+							href="https://displate.com"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							Displate
+						</a>
+					</figcaption>
+				</figure>
+				<section>
+					<h2 className="py-8 font-semibold text-xl">Film Summaries</h2>
+					<ol className="flex flex-col gap-y-8">
+						{filmSummaries.map((film) => (
+							<li key={film.id}>
+								<div>
+									<h3 className="py-2 font-semibold underline">{film.title}</h3>
+									<p>{film.summary}</p>
+								</div>
+							</li>
+						))}
+					</ol>
+				</section>
+			</main>
 		</div>
 	);
 }
