7b802bd6b9f4d216a7a25820e5abbdc37b2dbfed
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-01-12 09:36:55 +0700

    update libsql clients

--- data/clients/libsql-cf.ts
+++ data/clients/libsql-cf.ts
@@ -5,11 +5,15 @@ let client: Client;
 export function getClientCf(env: Env) {
 	let { TURSO_AUTH_TOKEN, TURSO_URL } = env;
 
-	if (!client || client.closed) {
+	if (!client) {
 		client = createClient({
 			authToken: TURSO_AUTH_TOKEN === '' ? undefined : TURSO_AUTH_TOKEN,
 			url: TURSO_URL,
 		});
+	}
+
+	if (client?.closed) {
+		client.reconnect();
 	}
 
 	return client;
--- data/clients/libsql-node.ts
+++ data/clients/libsql-node.ts
@@ -4,11 +4,15 @@ import { TURSO_AUTH_TOKEN, TURSO_URL } from '../../config/process-env';
 let client: Client;
 
 export function getClientNode() {
-	if (!client || client.closed) {
+	if (!client) {
 		client = createClient({
 			authToken: TURSO_AUTH_TOKEN === '' ? undefined : TURSO_AUTH_TOKEN,
 			url: TURSO_URL,
 		});
+	}
+
+	if (client?.closed) {
+		client.reconnect();
 	}
 
 	return client;
