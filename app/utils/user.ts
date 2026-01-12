fea0e5610fa0b6b0bf259f977d0958e39ff91f30
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-01-12 09:37:00 +0700

    add useUser fn to user module in utils

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -5,3 +5,13 @@ export function useOptionalUser() {
 	let data = useRouteLoaderData<typeof rootLoader>('root');
 	return data?.user ?? null;
 }
+
+export function useUser() {
+	let maybeUser = useOptionalUser();
+	if (!maybeUser) {
+		throw new Error(
+			'No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead.',
+		);
+	}
+	return maybeUser;
+}
