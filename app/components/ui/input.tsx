d9dda315c2afa05b7060be1c4e62a1af01d354ce
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-01-12 09:36:48 +0700

    fix base ui uncontrolled fieldcontrol error

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,19 +1,24 @@
 import { Input as BaseInput } from '@base-ui-components/react/input';
-import type * as React from 'react';
+import * as React from 'react';
 import { cn } from '@/app/utils/cn';
 
 function Input({
 	className,
+	defaultValue,
 	type = 'text',
 	...props
 }: React.ComponentProps<typeof BaseInput>) {
+	let [inputValue, setInputValue] = React.useState(defaultValue);
+
 	return (
 		<BaseInput
 			className={cn(
 				'flex w-full rounded-md border border-gray-2 bg-gray-1/25 px-3 py-3 focus-visible:border-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-primary-7 focus-visible:ring-inset disabled:cursor-not-allowed disabled:opacity-50 aria-[invalid]:border-destructive-5 aria-[invalid]:ring-destructive-5 aria-[invalid]:focus-visible:border-background aria-[invalid]:focus-visible:ring-2 aria-[invalid]:focus-visible:ring-inset',
 				className,
 			)}
+			onValueChange={(e) => setInputValue(e)}
 			type={type}
+			value={inputValue}
 			{...props}
 		/>
 	);
