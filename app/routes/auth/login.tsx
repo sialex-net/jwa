d1d254a4f24d3f5bdc6c882833903bbbe628e706
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-01-22 09:47:11 +0700

    add link to `/signup` route in `/login` route

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,8 +1,9 @@
 import { parseSubmission, report, useForm } from '@conform-to/react/future';
-import { Form, redirect, useSearchParams } from 'react-router';
+import { Form, Link, redirect, useSearchParams } from 'react-router';
 import { safeRedirect } from 'remix-utils/safe-redirect';
 import { z } from 'zod';
 import { ErrorList } from '@/app/components/forms';
+import { Spacer } from '@/app/components/spacer';
 import { Button } from '@/app/components/ui/button';
 import { Checkbox } from '@/app/components/ui/checkbox';
 import { Input } from '@/app/components/ui/input';
@@ -97,130 +98,143 @@ export default function Component({ actionData }: Route.ComponentProps) {
 	});
 
 	return (
-		<Form
-			className="mx-2 flex flex-col gap-y-2 pt-8"
-			method="POST"
-			{...form.props}
-		>
-			<div className="relative">
-				<Input
-					aria-describedby={
-						!fields.email.valid
-							? fields.email.errorId
-							: fields.email.descriptionId
-					}
-					aria-invalid={!fields.email.valid ? true : undefined}
-					className="peer pt-7 leading-5"
-					defaultValue={fields.email.defaultValue}
-					id={fields.email.id}
-					name={fields.email.name}
-					placeholder=""
-					type="email"
-				/>
-				<Label
-					className="peer-placeholder-shown:-translate-y-1/2 absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
-					htmlFor={fields.email.id}
-				>
-					Email
-				</Label>
-				<div
-					aria-hidden={true}
-					className="sr-only"
-					id={fields.email.descriptionId}
-				>
-					Please enter your email
+		<div className="container flex flex-col items-center">
+			<Form
+				className="container mx-2 flex max-w-xl flex-col gap-y-2 pt-8"
+				method="POST"
+				{...form.props}
+			>
+				<div className="relative">
+					<Input
+						aria-describedby={
+							!fields.email.valid
+								? fields.email.errorId
+								: fields.email.descriptionId
+						}
+						aria-invalid={!fields.email.valid ? true : undefined}
+						className="peer pt-7 leading-5"
+						defaultValue={fields.email.defaultValue}
+						id={fields.email.id}
+						name={fields.email.name}
+						placeholder=""
+						type="email"
+					/>
+					<Label
+						className="peer-placeholder-shown:-translate-y-1/2 absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
+						htmlFor={fields.email.id}
+					>
+						Email
+					</Label>
+					<div
+						aria-hidden={true}
+						className="sr-only"
+						id={fields.email.descriptionId}
+					>
+						Please enter your email
+					</div>
+					<div
+						aria-hidden={true}
+						className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
+						id={fields.email.errorId}
+					>
+						{fields.email.errors}
+					</div>
 				</div>
-				<div
-					aria-hidden={true}
-					className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
-					id={fields.email.errorId}
-				>
-					{fields.email.errors}
+				<div className="relative">
+					<Input
+						aria-describedby={
+							!fields.password.valid
+								? fields.password.errorId
+								: fields.password.descriptionId
+						}
+						aria-invalid={!fields.password.valid ? true : undefined}
+						className="peer pt-7 leading-5"
+						defaultValue={fields.password.defaultValue}
+						id={fields.password.id}
+						name={fields.password.name}
+						placeholder=""
+						type="password"
+					/>
+					<Label
+						className="peer-placeholder-shown:-translate-y-1/2 absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
+						htmlFor={fields.password.id}
+					>
+						Password
+					</Label>
+					<div
+						aria-hidden={true}
+						className="sr-only"
+						id={fields.password.descriptionId}
+					>
+						Please enter your password
+					</div>
+					<div
+						aria-hidden={true}
+						className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
+						id={fields.password.errorId}
+					>
+						{fields.password.errors}
+					</div>
 				</div>
-			</div>
-			<div className="relative">
-				<Input
-					aria-describedby={
-						!fields.password.valid
-							? fields.password.errorId
-							: fields.password.descriptionId
-					}
-					aria-invalid={!fields.password.valid ? true : undefined}
-					className="peer pt-7 leading-5"
-					defaultValue={fields.password.defaultValue}
-					id={fields.password.id}
-					name={fields.password.name}
-					placeholder=""
-					type="password"
-				/>
-				<Label
-					className="peer-placeholder-shown:-translate-y-1/2 absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
-					htmlFor={fields.password.id}
-				>
-					Password
-				</Label>
-				<div
-					aria-hidden={true}
-					className="sr-only"
-					id={fields.password.descriptionId}
-				>
-					Please enter your password
+				<div className="flex gap-x-3">
+					<Checkbox
+						aria-labelledby={fields.remember.descriptionId}
+						className="peer"
+						defaultChecked={fields.remember.defaultChecked}
+						id={fields.remember.id}
+						name={fields.remember.name}
+					/>
+					<Label
+						className="self-center font-light text-gray-4 hover:text-foreground peer-hover:text-foreground"
+						htmlFor={fields.remember.id}
+					>
+						Remember me? (14 days)
+					</Label>
+					<div
+						aria-hidden={true}
+						className="sr-only"
+						id={fields.remember.descriptionId}
+					>
+						Please check if you want your login to be remembered for 14 days
+					</div>
 				</div>
-				<div
-					aria-hidden={true}
-					className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
-					id={fields.password.errorId}
-				>
-					{fields.password.errors}
-				</div>
-			</div>
-			<div className="flex gap-x-3">
-				<Checkbox
-					aria-labelledby={fields.remember.descriptionId}
-					className="peer"
-					defaultChecked={fields.remember.defaultChecked}
-					id={fields.remember.id}
-					name={fields.remember.name}
-				/>
-				<Label
-					className="self-center font-light text-gray-4 hover:text-foreground peer-hover:text-foreground"
-					htmlFor={fields.remember.id}
-				>
-					Remember me? (14 days)
-				</Label>
-				<div
-					aria-hidden={true}
-					className="sr-only"
-					id={fields.remember.descriptionId}
-				>
-					Please check if you want your login to be remembered for 14 days
-				</div>
-			</div>
 
-			<input
-				defaultValue={fields.redirectTo.defaultValue}
-				id={fields.redirectTo.id}
-				key={fields.redirectTo.key}
-				name={fields.redirectTo.name}
-				type="hidden"
-			/>
+				<input
+					defaultValue={fields.redirectTo.defaultValue}
+					id={fields.redirectTo.id}
+					key={fields.redirectTo.key}
+					name={fields.redirectTo.name}
+					type="hidden"
+				/>
 
-			<ErrorList
-				errors={form.errors}
-				id={form.errorId}
-			/>
+				<ErrorList
+					errors={form.errors}
+					id={form.errorId}
+				/>
 
-			<div className="py-2">
-				<Button
-					aria-label="Log in"
-					className="w-full text-lg"
-					size="default"
-					type="submit"
+				<div className="py-2">
+					<Button
+						aria-label="Log in"
+						className="w-full text-lg"
+						size="default"
+						type="submit"
+					>
+						Login
+					</Button>
+				</div>
+			</Form>
+			<Spacer size="xs" />
+			<p>
+				New User?{' '}
+				<Link
+					className="underline hover:no-underline"
+					prefetch="intent"
+					to="/signup"
 				>
-					Login
-				</Button>
-			</div>
-		</Form>
+					Sign Up
+				</Link>
+			</p>
+		</div>
 	);
 }
 
