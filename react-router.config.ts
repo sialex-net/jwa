e835ab025340e9c83ff26b7acd5085a1ac4b4c9d
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-01-22 09:47:15 +0700

    change future unstable flags to v8 in react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,9 +3,9 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	future: {
 		unstable_optimizeDeps: true,
-		unstable_splitRouteModules: true,
-		unstable_viteEnvironmentApi: true,
 		v8_middleware: true,
+		v8_splitRouteModules: true,
+		v8_viteEnvironmentApi: true,
 	},
 	ssr: true,
 } satisfies Config;
