98206763e4ab3a2684a69e43f96dc7aec4ecfb9c
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-02-06 05:43:35 +0700

    update `/signup` route

--- app/routes/auth/signup.tsx
+++ app/routes/auth/signup.tsx
@@ -7,54 +7,21 @@ import { z } from 'zod';
 import { ErrorList } from '@/app/components/forms';
 import { Spacer } from '@/app/components/spacer';
 import { Button } from '@/app/components/ui/button';
-import { Checkbox } from '@/app/components/ui/checkbox';
 import { Input } from '@/app/components/ui/input';
 import { Label } from '@/app/components/ui/label';
 import { appContext, getContext } from '@/app/context';
 import { connectClientCf } from '@/app/middleware/libsql';
-import { requireAnonymous, sessionKey, signup } from '@/app/utils/auth.server';
-import { getSessionStorage } from '@/app/utils/sessions.server';
-import {
-	EmailSchema,
-	PasswordSchema,
-	UsernameSchema,
-} from '@/app/utils/user-validation';
+import { requireAnonymous } from '@/app/utils/auth.server';
+import { sendEmail } from '@/app/utils/email.server';
+import { EmailSchema } from '@/app/utils/user-validation';
+import { getVerifySessionStorage } from '@/app/utils/verification.server';
 import * as schema from '@/data/drizzle/schema';
 import type { Route } from './+types/signup';
+import { onboardingEmailSessionKey } from './onboarding';
 
-const SignupFormSchema = z
-	.object({
-		agreeToTermsOfServiceAndPrivacyPolicy: z.preprocess(
-			(v) => v === 'on',
-			z.boolean(),
-		),
-		confirmPassword: PasswordSchema,
-		email: EmailSchema,
-		password: PasswordSchema,
-		remember: z.preprocess((v) => v === 'on', z.boolean()),
-		username: UsernameSchema,
-	})
-	.superRefine(
-		(
-			{ confirmPassword, password, agreeToTermsOfServiceAndPrivacyPolicy },
-			ctx,
-		) => {
-			if (confirmPassword !== password) {
-				ctx.addIssue({
-					code: 'custom',
-					message: 'Passwords must match',
-					path: ['confirmPassword'],
-				});
-			}
-
-			if (!agreeToTermsOfServiceAndPrivacyPolicy) {
-				ctx.addIssue({
-					code: 'custom',
-					message: 'You must agree to the terms of service and privacy policy',
-				});
-			}
-		},
-	);
+const SignupFormSchema = z.object({
+	email: EmailSchema,
+});
 
 export async function loader({ context, request }: Route.LoaderArgs) {
 	let { env } = getContext(context, appContext);
@@ -78,12 +45,6 @@ export async function action({ context, request }: Route.ActionArgs) {
 			.where(eq(schema.users.email, data.email))
 			.get();
 
-		let existingUsername = await db
-			.select({ id: schema.users.id })
-			.from(schema.users)
-			.where(eq(schema.users.username, data.username))
-			.get();
-
 		if (existingEmail) {
 			ctx.addIssue({
 				code: 'custom',
@@ -91,17 +52,6 @@ export async function action({ context, request }: Route.ActionArgs) {
 				path: ['email'],
 			});
 		}
-
-		if (existingUsername) {
-			ctx.addIssue({
-				code: 'custom',
-				message: 'Username not available',
-				path: ['username'],
-			});
-		}
-	}).transform(async (data) => {
-		let session = await signup(data);
-		return { ...data, session };
 	});
 
 	let result = await superRefined.safeParseAsync(submission.payload);
@@ -113,27 +63,43 @@ export async function action({ context, request }: Route.ActionArgs) {
 					error: {
 						issues: result.error.issues,
 					},
-					hideFields: ['confirmPassword', 'password'],
 				}),
 			},
 			{ status: 400 },
 		);
 	}
 
-	let { remember, session } = result.data;
+	let { email } = result.data;
 
-	let cookieSession = await getSessionStorage(env).getSession(
-		request.headers.get('cookie'),
-	);
-	cookieSession.set(sessionKey, session.id);
-
-	return redirect('/', {
-		headers: {
-			'set-cookie': await getSessionStorage(env).commitSession(cookieSession, {
-				expires: remember ? session.expirationDate : undefined,
-			}),
-		},
+	let response = await sendEmail(env, {
+		subject: 'Welcome to John Wicki',
+		text: 'test',
+		to: email,
 	});
+
+	if (response?.status === 'success') {
+		const verifySession = await getVerifySessionStorage(env).getSession(
+			request.headers.get('cookie'),
+		);
+		verifySession.set(onboardingEmailSessionKey, email);
+		return redirect('/onboarding', {
+			headers: {
+				'set-cookie':
+					await getVerifySessionStorage(env).commitSession(verifySession),
+			},
+		});
+	} else {
+		return data(
+			{
+				result: report(submission, {
+					error: {
+						formErrors: [response.error],
+					},
+				}),
+			},
+			{ status: 500 },
+		);
+	}
 }
 
 export const meta: MetaFunction = () => {
@@ -154,7 +120,7 @@ export default function Component({ actionData }: Route.ComponentProps) {
 		<div className="container flex flex-col items-center">
 			<header className="container flex flex-col items-center gap-3">
 				<h1 className="text-2xl">Account Sign-up</h1>
-				<p className="text-muted-foreground">Please enter your details</p>
+				<p className="text-muted-foreground">Please enter your email</p>
 			</header>
 			<Spacer size="xs" />
 			<Form
@@ -200,172 +166,6 @@ export default function Component({ actionData }: Route.ComponentProps) {
 						{fields.email.errors}
 					</div>
 				</div>
-				<div className="relative">
-					<Input
-						aria-describedby={
-							!fields.username.valid
-								? fields.username.errorId
-								: fields.username.descriptionId
-						}
-						aria-invalid={!fields.username.valid ? true : undefined}
-						autoComplete="username"
-						className="peer pt-7 lowercase leading-5"
-						defaultValue={fields.username.defaultValue}
-						id={fields.username.id}
-						name={fields.username.name}
-						placeholder=""
-						type="text"
-					/>
-					<Label
-						className="absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
-						htmlFor={fields.username.id}
-					>
-						Username
-					</Label>
-					<div
-						aria-hidden={true}
-						className="sr-only"
-						id={fields.email.descriptionId}
-					>
-						Please enter a username
-					</div>
-					<div
-						aria-hidden={true}
-						className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
-						id={fields.username.errorId}
-					>
-						{fields.username.errors}
-					</div>
-				</div>
-				<div className="relative">
-					<Input
-						aria-describedby={
-							!fields.password.valid
-								? fields.password.errorId
-								: fields.password.descriptionId
-						}
-						aria-invalid={!fields.password.valid ? true : undefined}
-						autoComplete="new-password"
-						className="peer pt-7 leading-5"
-						defaultValue={fields.password.defaultValue}
-						id={fields.password.id}
-						name={fields.password.name}
-						placeholder=""
-						type="password"
-					/>
-					<Label
-						className="absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
-						htmlFor={fields.password.id}
-					>
-						Password
-					</Label>
-					<div
-						aria-hidden={true}
-						className="sr-only"
-						id={fields.password.descriptionId}
-					>
-						Please enter a password
-					</div>
-					<div
-						aria-hidden={true}
-						className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
-						id={fields.password.errorId}
-					>
-						{fields.password.errors}
-					</div>
-				</div>
-				<div className="relative">
-					<Input
-						aria-describedby={
-							!fields.confirmPassword.valid
-								? fields.confirmPassword.errorId
-								: fields.confirmPassword.descriptionId
-						}
-						aria-invalid={!fields.confirmPassword.valid ? true : undefined}
-						autoComplete="new-password"
-						className="peer pt-7 leading-5"
-						defaultValue={fields.confirmPassword.defaultValue}
-						id={fields.confirmPassword.id}
-						name={fields.confirmPassword.name}
-						placeholder=""
-						type="password"
-					/>
-					<Label
-						className="absolute top-2 left-4 font-light text-gray-4 text-xs peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-lg peer-hover:text-gray-7 peer-focus-visible:pb-7.5 peer-focus-visible:text-foreground peer-focus-visible:text-xs peer-focus-visible:hover:text-foreground"
-						htmlFor={fields.confirmPassword.id}
-					>
-						Confirm Password
-					</Label>
-					<div
-						aria-hidden={true}
-						className="sr-only"
-						id={fields.confirmPassword.descriptionId}
-					>
-						Please confirm password
-					</div>
-					<div
-						aria-hidden={true}
-						className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
-						id={fields.confirmPassword.errorId}
-					>
-						{fields.confirmPassword.errors}
-					</div>
-				</div>
-				<div className="flex gap-x-3 py-0.5">
-					<Checkbox
-						aria-labelledby={
-							fields.agreeToTermsOfServiceAndPrivacyPolicy.descriptionId
-						}
-						className="peer"
-						defaultChecked={
-							fields.agreeToTermsOfServiceAndPrivacyPolicy.defaultChecked
-						}
-						id={fields.agreeToTermsOfServiceAndPrivacyPolicy.id}
-						name={fields.agreeToTermsOfServiceAndPrivacyPolicy.name}
-					/>
-					<Label
-						className="self-center font-light text-gray-4 hover:text-foreground peer-hover:text-foreground"
-						htmlFor={fields.agreeToTermsOfServiceAndPrivacyPolicy.id}
-					>
-						Do you agree to our Terms of Service and Privacy Policy?
-					</Label>
-					<div
-						aria-hidden={true}
-						className="sr-only"
-						id={fields.agreeToTermsOfServiceAndPrivacyPolicy.descriptionId}
-					>
-						Please check if you agree to terms of service and privacy policy
-					</div>
-					<div
-						aria-hidden={true}
-						className="absolute right-4 bottom-1 font-light text-destructive-5 text-xs"
-						id={fields.agreeToTermsOfServiceAndPrivacyPolicy.errorId}
-					>
-						{fields.agreeToTermsOfServiceAndPrivacyPolicy.errors}
-					</div>
-				</div>
-				<div className="flex gap-x-3 py-0.5">
-					<Checkbox
-						aria-labelledby={fields.remember.descriptionId}
-						className="peer"
-						defaultChecked={fields.remember.defaultChecked}
-						id={fields.remember.id}
-						name={fields.remember.name}
-					/>
-					<Label
-						className="self-center font-light text-gray-4 hover:text-foreground peer-hover:text-foreground"
-						htmlFor={fields.remember.id}
-					>
-						Remember me? (14 days)
-					</Label>
-					<div
-						aria-hidden={true}
-						className="sr-only"
-						id={fields.remember.descriptionId}
-					>
-						Please check if you want your login to be remembered for 14 days
-					</div>
-				</div>
 				<div className="flex min-h-12 flex-col gap-y-2 pt-1">
 					<ErrorList
 						errors={form.errors}
@@ -396,7 +196,7 @@ export default function Component({ actionData }: Route.ComponentProps) {
 						className="w-full text-lg"
 						type="submit"
 					>
-						Create an account
+						Submit
 					</Button>
 				</div>
 			</Form>
