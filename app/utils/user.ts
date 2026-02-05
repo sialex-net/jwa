102136dadb3a214e4f49ec1e444f7ab475293730
sialex-net <91857463+sialex-net@users.noreply.github.com>
2026-02-05 07:54:24 +0700

    add fns to user module in utils

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -15,3 +15,43 @@ export function useUser() {
 	}
 	return maybeUser;
 }
+
+export function userHasPermission(
+	user: null | Pick<ReturnType<typeof useUser>, 'roles'>,
+	permission: PermissionString,
+) {
+	if (!user) return false;
+	const { action, entity, access } = parsePermissionString(permission);
+	return user.roles.some((role) =>
+		role.permissions.some(
+			(permission) =>
+				permission.entity === entity &&
+				permission.action === action &&
+				(!access || access.includes(permission.access)),
+		),
+	);
+}
+
+export function userHasRole(
+	user: null | Pick<ReturnType<typeof useUser>, 'roles'>,
+	role: string,
+) {
+	if (!user) return false;
+	return user.roles.some((r) => r.name === role);
+}
+
+type Action = 'create' | 'delete' | 'read' | 'update';
+type Entity = 'note' | 'user';
+type Access = 'any' | 'any,own' | 'own' | 'own,any';
+export type PermissionString =
+	| `${Action}:${Entity}:${Access}`
+	| `${Action}:${Entity}`;
+
+export function parsePermissionString(permissionString: PermissionString) {
+	const [action, entity, access] = permissionString.split(':');
+	return {
+		access: access ? access.split(',') : undefined,
+		action,
+		entity,
+	};
+}
